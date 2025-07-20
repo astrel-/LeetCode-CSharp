14b607d7edddc4af479573973ab13e9e01ce7439
Yury <yury.baryshnikov137@gmail.com>
2025-07-20 20:31:40 +0100

    Generic Problem

---
+++ Problem000.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using Assert = Xunit.Assert;
+
+namespace LeetCode;
+
+public class Problem000
+{
+    public object Solve(object o)
+    {
+        return "";
+    }
+}
+
+public static class Problem000Test
+{
+    public static IEnumerable<object[]> TestData()
+    {
+        yield return ["", ""];
+    }
+
+    [TestCaseSource(nameof(TestData))]
+    public static void Test(string input, string output)
+    {
+        var problem = new Problem000();
+        Assert.Equal(output, problem.Solve(input));
+    }
+}
