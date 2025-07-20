bf34fbef8f6139d5bbf7fe860a59358b9ec8ac71
Yury <yury.baryshnikov137@gmail.com>
2025-07-20 20:27:03 +0100

    005 + 006

---
+++ Problem005.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using Assert = Xunit.Assert;
+
+namespace LeetCode;
+
+public class Problem005
+{
+    public string LongestPalindrome(string s)
+    {
+        return "abc";
+    }
+}
+
+public static class Problem005Test
+{
+    public static IEnumerable<object[]> TestData()
+    {
+        yield return ["babad", "bab"];
+        yield return ["cbbd", "bb"];
+    }
+
+    [TestCaseSource(nameof(TestData))]
+    public static void Test(string input, string output)
+    {
+        var problem = new Problem005();
+        Assert.Equal(output, problem.LongestPalindrome(input));
+    }
+}
---
+++ Problem006.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using Assert = Xunit.Assert;
+
+namespace LeetCode;
+
+public class Problem006
+{
+    public string Convert(string s, int numRows)
+    {
+        if (numRows <= 1)
+            return s;
+        // if (s.Length <= numRows)
+        //     return s;
+        var res = new char[s.Length];
+        var k = 2 * numRows - 2;
+        var idx = 0;
+        for (var row = 0; row < numRows; row++)
+        {
+            for (var col = 0; col < s.Length + k/2; col += k)
+            {
+                var undIdx = col - row;
+                if (undIdx >= 0 && undIdx < s.Length)
+                {
+                    res[idx] = s[undIdx];
+                    idx++;
+                }
+
+                undIdx = col + row;
+                if (row > 0 && row < (numRows - 1) && undIdx >= 0 && undIdx < s.Length)
+                {
+                    res[idx] = s[undIdx];
+                    idx++;
+                }
+            }
+        }
+
+        return new string(res);
+    }
+}
+
+public static class Problem006Test
+{
+    public static IEnumerable<object[]> TestData()
+    {
+        yield return [("PAYPALISHIRING", 3), "PAHNAPLSIIGYIR"];
+        yield return [("PAYPALISHIRING", 4), "PINALSIGYAHRPI"];
+        yield return [("A", 1), "A"];
+        yield return [("AB", 2), "AB"];
+        yield return [("ABCD", 2), "ACBD"];
+    }
+
+    [TestCaseSource(nameof(TestData))]
+    public static void Test((string InputString, int NumRows) input, string output)
+    {
+        var problem = new Problem006();
+        Assert.Equal(output, problem.Convert(input.InputString, input.NumRows));
+    }
+}
