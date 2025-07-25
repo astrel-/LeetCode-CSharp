b41da50abe5f0e1b00ce6f3f0eeaa0f1c652062f
Yury <yury.baryshnikov137@gmail.com>
2025-07-25 22:06:15 +0100

    0118 - naive

---
+++ Problem0118.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode;
+
+public class Problem0118
+{
+    public IList<IList<int>> Generate(int numRows) 
+    {
+        if  (numRows == 1)
+            return new List<IList<int>> {new List<int> {1}};
+        
+        var prev = Generate(numRows - 1);
+        var last = prev[^1];
+        var curr = last.Zip(last.Skip(1)).Select(pair => pair.First + pair.Second).Prepend(1).Append(1).ToList();
+        prev.Add(curr);
+        return prev;
+    }
+}
