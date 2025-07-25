7257b6ba858576e691aad6993f348477e486b5cf
Yury <yury.baryshnikov137@gmail.com>
2025-07-25 22:34:23 +0100

    0119 - naive

---
+++ Problem0119.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode;
+
+public class Problem0119
+{
+    public IList<int> GetRow(int rowIndex) {
+        
+        var curr = new long[rowIndex+1];
+        curr[0] = 1;
+        curr[rowIndex] = 1;
+        long num = 1;
+        for (var i = 1; i < rowIndex; i++)
+        {
+            num *= rowIndex + 1 - i;
+            num /= i;
+            curr[i] = num;
+        }
+            
+        return curr.Select(i => (int)i).ToList();
+    }
+}
