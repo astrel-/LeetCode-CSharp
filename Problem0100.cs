04f1583b8004cde7dd02607070f670bc489e616b
Yury <yury.baryshnikov137@gmail.com>
2025-07-25 21:45:05 +0100

    0100

---
+++ Problem0100.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode;
+
+public class Problem0100
+{
+    public bool IsSameTree(TreeNode p, TreeNode q) {
+        if (p is null && q is null)
+            return true;
+
+        if (p is null || q is null)
+            return false;
+
+        return (p.val == q.val && IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right));
+    }
+}
