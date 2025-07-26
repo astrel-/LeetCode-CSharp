a18b04b8e24f70bb27711f30c31058e4cb3f363d
Yury <yury.baryshnikov137@gmail.com>
2025-07-26 12:05:48 +0100

    0027

---
+++ Problem0027.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode;
+
+public class Problem0027
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        var idx = 0;
+        foreach (var num in nums)
+        {
+            if (num != val)
+            {
+                nums[idx++] = num;
+            }
+        }
+        return idx;    
+    }
+}
