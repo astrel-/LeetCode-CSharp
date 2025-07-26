912875bf7b5657753de4123ac88aadb2893acb54
Yury <yury.baryshnikov137@gmail.com>
2025-07-26 12:02:30 +0100

    0026

---
+++ Problem0026.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode;
+
+public class Problem0026
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        if (nums.Length == 0)
+            return 0;
+        var idx = 1;
+        var curr = nums[0];
+        foreach (var num in nums.Skip(1))
+        {
+            if (num != curr)
+            {
+                curr = num;
+                nums[idx++] = curr;
+            }
+        }
+        return idx;
+    }
+}
