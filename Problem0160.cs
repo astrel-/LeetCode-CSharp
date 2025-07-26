397052f4dff572019f96fad2c5eada27c2def07e
Yury <yury.baryshnikov137@gmail.com>
2025-07-26 11:34:31 +0100

    0160

---
+++ Problem0160.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode;
+
+public class Problem0160
+{
+    public ListNode GetIntersectionNode(ListNode headA, ListNode headB) {
+
+        var a = headA;
+        var b = headB;
+
+        while (a!=b)
+        {
+            a = a is not null ? a.next : headB;
+            b = b is not null ? b.next : headA;
+        }
+
+        return a;
+    }
+}
