e2562775f6624a6e21a986ce5560fd9b9957a773
Yury <yury.baryshnikov137@gmail.com>
2025-07-26 10:41:31 +0100

    0137

---
+++ Problem0137.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode;
+
+public class Problem0137
+{
+    public int SingleNumber(int[] nums) {
+        var ones = 0;
+        var twos = 0;
+
+        foreach (var num in nums) {
+            ones ^= (num & ~twos);
+            twos ^= (num & ~ones);
+        }
+
+        return ones;
+    }
+}
