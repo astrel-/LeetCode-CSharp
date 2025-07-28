127764746ddad02a22931841598120484dede3fe
Yury <yury.baryshnikov137@gmail.com>
2025-07-28 18:37:16 +0100

    0933 - using built in queues

---
+++ Problem0933.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using Assert = Xunit.Assert;
+
+namespace LeetCode;
+
+public class Problem0933
+{
+    public class RecentCounter
+    {
+        private const int MAX_TIME = 3000;
+        private Queue<int> _queue = new();
+
+        public int Ping(int t)
+        {
+            _queue.Enqueue(t);
+            while (_queue.Count != 0 && _queue.Peek() < t - MAX_TIME)
+            {
+                _queue.Dequeue();
+            }
+            return _queue.Count;
+        }
+    }
+}
